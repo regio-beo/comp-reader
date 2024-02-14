4d1565b461b028d8f6699945acf0ff7cb8e06b81
Benjamin Fankhauser <benjamin.fankhauser@students.unibe.ch>
2024-02-14 18:30:49 +0100

    monte carlo simulation of outcomes

--- compreader/competition.py
+++ compreader/competition.py
@@ -1,3 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from tqdm import tqdm
 
 class Competition:
 
@@ -15,6 +20,161 @@ class Competition:
         p = Pilot(name)
         self.pilots.append(p)
         return p
+
+    def simulateMinMaxScores(self, n, day_quality, strategy):
+        self.applyScoring(strategy) # compute current scores
+        for p in self.pilots:
+            p.current_score = p.total_points
+        
+        for _ in range(n):
+            self.addVirtualTask(day_quality, 0)        
+        self.applyScoring(strategy) # compute minimum scores
+        for p in self.pilots:
+            p.min_score = p.total_points
+        # compute improvement performance:
+        for p in self.pilots:
+            p.min_improvement_performance = 1.0            
+            for score in p.ftv_scores:                
+                if p.min_improvement_performance == 1.0 and score.usage_ftv_int < 100:
+                    p.min_improvement_performance = score.performance
+            p.min_improvement_performance = int (p.min_improvement_performance * day_quality * 1000) # convert to day quality                   
+        
+        self.tasks = self.tasks[:-n] # remove min scoring task
+        for _ in range(n):
+            self.addVirtualTask(day_quality, day_quality*1000)
+        self.applyScoring(strategy) # compute max scores
+        for p in self.pilots:
+            p.max_score = p.total_points        
+        
+        # compute positions:
+        max_scores = np.array([p.max_score for p in self.pilots])
+        for p in self.pilots:
+            p.min_position = (max_scores > p.min_score).sum() # +1 not required as yourself are included in the better pilots
+        # missing points to reach:
+        
+        min_scores = np.array(sorted([p.min_score for p in self.pilots], reverse=True))
+        for p in self.pilots:
+            p.max_position = (min_scores > p.max_score).sum()+1 # +1 as you are lower than yourself
+        for p in self.pilots:
+            p.max_position_difference = int(day_quality*1000 + min_scores[p.max_position-1] - p.max_score) # compute missing points to achieve max position
+
+        self.printMinMaxScores()
+
+        self.tasks = self.tasks[:-n] # remove max scoring task (cleanup)
+    
+    def monteCarloSimulation(self, n, n_tasks, day_quality, method, strategy, order_as_initial=True, limit_plot=50):
+
+        # recreate current situation:
+        self.applyScoring(strategy) # might be important
+        if order_as_initial: # shows the ranking with the information available at the time
+            self.pilots = sorted(self.pilots, key=lambda p: p.total_points, reverse=True)
+        n_flown_task = len(self.tasks)
+        
+        # run simulation
+        n_pilots = len(self.pilots)
+        counter = np.zeros((n_pilots, n_pilots))
+        for i in range(n_pilots):
+            self.pilots[i].counter_id = i # helper to find counting row
+        
+        # compute mu and std of pilot performances:
+        for p in self.pilots:
+            performances = []
+            for score in p.ftv_scores:
+                if score.performance_int > 0:
+                    performances.append(score.performance)
+            if len(performances) > 0:
+                p.performance_mu = np.mean(performances)
+                p.performance_sigma = np.std(performances)
+            else:
+                # defaults?! average of population?
+                p.performance_mu = 0.1
+                p.performance_sigma = 0.01
+            #print(f'{p.name:>20}: {p.performance_mu:.2f}, +-{p.performance_sigma:.2f}')
+
+        prev_counter = None
+        for i in tqdm(range(n)):
+            for _ in range(n_tasks):
+                t_monte = self.addTask('MonteCarlo', day_quality)
+                for p in self.pilots:                   
+                    # uniform points:
+                    if method == 'uniform':
+                        points = np.random.randint(0, int(day_quality*1000))
+                    
+                    # random selected (previous) performance:
+                    if method == 'copy_task':
+                        task_like = np.random.randint(0, n_flown_task)
+                        max_points = int(p.ftv_scores[task_like].performance*day_quality*1000)
+                        points = np.random.randint(0, max_points+1)
+
+                    #pilots do not exceed their max performance:
+                    if method == 'uniform_max_pb':
+                        points = np.random.randint(0, int(p.ftv_scores[0].performance*day_quality*1000)+1)
+
+                    # skill based mu sigma performance:
+                    if method == 'gaussian_pilot':
+                        performance = np.random.normal(loc=p.performance_mu, scale=p.performance_sigma)
+                        performance = min(1.0, performance)
+                        performance = max(0.0, performance)
+                        points = int(performance*day_quality*1000)
+
+                    t_monte.addParticipation(p, points)
+
+            self.applyScoring(strategy)
+            monte_pilots = sorted(self.pilots, key=lambda p: p.total_points, reverse=True)
+            for pos,pilot in enumerate(monte_pilots):
+                counter[pilot.counter_id, pos] += 1
+            self.tasks = self.tasks[:-n_tasks] # remove monte task
+
+            if i % 100 == 0:        
+                counter_normalized = counter.copy()/(i+1)         
+                if prev_counter is not None:
+                    change = np.mean((prev_counter-counter_normalized)**2)
+                    #print('change:', change)
+                    if change < 1e-7:
+                        n = i # fix early stopping
+                        break
+                prev_counter = counter_normalized
+
+            #    print(counter/(i+1))
+        
+        print('~~~ Final Simulation ~~~')
+        #for p in self.pilots:
+        #    mx = np.argmax(counter[p.counter_id]) + 1
+        #    print(f'{p.name}: {counter[p.counter_id]}, Expected Rank: {mx}')
+        
+        # normalize per row:
+        mxs = np.max(counter, 1)
+        counter_normalized = (counter.T/mxs).T
+        counter_probs = counter/n
+        #print(counter_normalized)
+        # as probabilities:
+        #counter_normalized = counter/n
+
+        limit_plot = min(n_pilots, limit_plot)
+
+        names = [p.name for p in self.pilots][:limit_plot]
+
+        # create plot:
+        mpl.rcParams['figure.dpi'] = 75
+        fig, ax = plt.subplots()
+        im = ax.imshow(counter_normalized[:limit_plot, :limit_plot])
+        ax.plot([0, limit_plot-1], [0, limit_plot-1], linestyle='dashed', color='r', alpha=0.6)
+
+        # set labels
+        ax.set_xticks(np.arange(limit_plot), labels=np.arange(limit_plot)+1) # start rank at 1
+        ax.set_yticks(np.arange(limit_plot), labels=names)
+
+        # print normalized values:
+        for i in range(limit_plot):
+            for j in range(limit_plot):
+                text = ax.text(j, i, f'{counter_probs[i, j]:.2f}', ha='center', va='center', color='w', size=6)
+
+        ax.set_title(f'Monte Carlo Simulation of {n} different Task outcomes')
+        #fig.tight_layout
+        plt.show()        
+
+        #print(counter/(i+1))
+
     
     def addVirtualTask(self, quality, points):
         tv = self.addTask('TV', quality)
@@ -45,17 +205,38 @@ class Competition:
     def printFtvScores(self):
         print(f'FTV scores of {self.name}')
         pilots = sorted(self.pilots, key=lambda p: p.total_points, reverse=True)
-        for p in pilots:
+        for p in pilots[:5]:
             p.print_ftv_scores()
+    
+    def printMinMaxScores(self):
+        print(f'Extrema Scores of {self.name}:')
+        print('                     Name | current    | min              | max ')
+        pilots = sorted(self.pilots, key=lambda p: p.current_score, reverse=True)
+        for p in pilots:
+            p.print_min_max_scores()
+        
 
 
 class Pilot:
 
     def __init__(self, name):
+        # clean name
+        name = name.replace("\n", " ")
+        name = name.replace("\r", " ")
+        name = name.replace("\t", " ")        
+
         self.name = name
         self.total_points = 0
         self.fixed_total_validity = 0
         self.ftv_scores = []
+
+        # compute current/min/max scores
+        self.current_score = 0 # the current scores
+        self.min_score = 0 # if the next run is 0 points
+        self.min_position = 0 # what if all other score max points?
+        self.min_improvement_performance = 0 # what to achieve in order to improve min_score (relative to )
+        self.max_score = 0 # what if next run is 1000 points
+        self.max_position = 0 # what if all others score 0 points?
     
     def print(self):
         print(f'{self.name}: {self.total_points}')
@@ -66,16 +247,23 @@ class Pilot:
     def print_ftv_scores(self):
         # first row: name | task: points/winner (performance) | ... | total points
         # second row:   ~ |
-        first_row = [f'{self.name:>20}']
+        first_row = [f'{self.name:>25}']
         first_row += [f'{s.task.name:>5}: {s.points:>4}/{s.winner_score:>4} ({s.performance_int:>3}%)' for s in self.ftv_scores]
         first_row += [f'{self.total_points:>5}']
-        second_row = [f'{"":>20}']
+        second_row = [f'{"":>25}']
         second_row += [f'{s.left_out:>15}  ({s.usage_ftv_int:>3}%)' for s in self.ftv_scores] # 23
         total_left_out = self.fixed_total_validity - self.total_points
         second_row += [f'{total_left_out:>5}']
         print('|'.join(first_row))
         print('|'.join(second_row))
         print('')
+
+    def print_min_max_scores(self):
+        # compute usage score to improve on minimum:
+
+        print(f'{self.name:>25} | {self.current_score:>10} |' \
+            + f'{self.min_score:>5} ({self.min_position:>3}) >{self.min_improvement_performance:>3} |'\
+            + f'{self.max_score:>5} ({self.max_position:>3}) >{self.max_position_difference:>3}')
 
 
 class Task:
@@ -89,10 +277,15 @@ class Task:
         self.participants.append(Participant(pilot, self, points))
 
     def winnerScore(self):
-        if self.name == 'TV':
-            return (self.quality*1000) # special case
-        scores = [p.points for p in self.participants]
-        return max(scores)
+        return round(self.quality*1000)
+
+    def winnerScorebyParticipant(self):
+        #return int(self.quality*1000)
+        #if self.name == 'TV':
+        #    return int(self.quality*1000) # special case
+        #scores = [p.points for p in self.participants]
+        #return max(scores)
+        pass # do not use this function
     
 class Participant:
 
--- compreader/competition_reader.py
+++ compreader/competition_reader.py
@@ -4,13 +4,13 @@ import pandas as pd
 from competition import Competition, Pilot, Task, Participant 
 from ftv import FixedTotalValidityStrategy
 
-PAGES = "1" # "all"
+PAGES = ["1", "1,2", "1,2,3", "all"][3]
 
 class CompetitionFile:
 
     def __init__(self, name, path):
         self.name = name
-        self.path = path
+        self.path = path        
     
     def read(self):
         tables = camelot.read_pdf(self.path, pages=PAGES)
@@ -22,20 +22,34 @@ class CompetitionFile:
             print(df)
 
     def read_competition(self):
-        tables = camelot.read_pdf(self.path, pages=PAGES)       
+        tables = camelot.read_pdf(self.path, pages=PAGES)
+
+        # skip 3x3 tables at the beginning:
+        offset_table = 0
+        if tables[0].shape == (3, 3):
+            offset_table = 1               
         
         # read task table
-        df_tasks = tables[1].df
+        df_tasks = tables[offset_table].df
         #df_tasks.columns = df_tasks.iloc[0]
         df_tasks.columns = ['Task', 'Date', 'Distance', 'Validity', 'Type']
         df_tasks = df_tasks[1:]
         df_tasks.reset_index(inplace=True, drop=True)
+        for idx in df_tasks.index:
+            entry = df_tasks.at[idx, 'Validity']
+            validity = float(entry[:-1])/100
+            df_tasks.at[idx, 'Validity'] = validity
         n_tasks = len(df_tasks)
 
         # append additional tables:                
-        results = [tables[i].df[1:] for i in range(2, len(tables))]
+        results = [tables[i].df[1:] for i in range(offset_table+1, len(tables))]
         result = pd.concat(results)
-        result.columns = tables[2].df.iloc[0]
+        columns = list(tables[offset_table+1].df.iloc[0])
+        # fix columns at Tasks:
+        columns[3] = 'Sex'    
+        task_names = [f'T_{i+1}' for i in range(n_tasks)]
+        result.columns = columns[:7] + task_names + columns[7+n_tasks:]      
+            
         result.reset_index(inplace=True, drop=True)
 
         # convert data types
@@ -46,9 +60,7 @@ class CompetitionFile:
                     pos = entry.index('/')                    
                     result.at[idx, result.columns[7+i]] = entry[pos+1:]
                 except ValueError:
-                    pass
-
-            
+                    pass            
                 
         for i in range(n_tasks):
             result.isetitem(7+i, pd.to_numeric(result.iloc[:, 7+i]))
@@ -61,8 +73,8 @@ class CompetitionFile:
 
         # create tasks:
         task_names = [result.iloc[:, 7+i].name for i in range(n_tasks)]
-        task_validities = [int(result.iloc[:, 7+i].max())/1000. for i in range(n_tasks)]
-        tasks = [competition.addTask(n, v) for n, v in zip(task_names, task_validities)]
+        #task_validities = [int(result.iloc[:, 7+i].max())/1000. for i in range(n_tasks)]        
+        tasks = [competition.addTask(n, float(v)) for n, v in zip(task_names, df_tasks[['Validity']].values.flatten())]
 
         # register participants:
         for i,t in enumerate(tasks):
@@ -120,12 +132,48 @@ if __name__ == "__main__":
 
     # Alpen Cup
     #path = "files/alpen-cup/AC_SportsClass_V1.pdf" # does not work of image format
-    path = "files/alpen-cup/AC_SportsClass_V4.pdf"
-    file = CompetitionFile("Alpen Cup", path)
-    #file.read()
+    #path = "files/alpen-cup/AC_SportsClass_V4.pdf"
+    #file = CompetitionFile("Alpen Cup", path)
+    #ftv = 0.25
+
+    # Swiss Cup
+    #path = "files/swiss-cup/SC-SportsClass-V11.pdf"
+    path = "files/swiss-cup/SC-Overall-V11.pdf"
+    file = CompetitionFile("Swiss Cup", path)
+    ftv = 0.4 if "SportsClass" in path else 0.3    
+
+    # OGO:
+    #path = "files/ogo/OGO_SportsClass_V5.pdf"
+    #file = CompetitionFile("OGO", path)
+    #ftv = 0.25
+
+    # min max simulation
     competition = file.read_competition()
-    competition.addVirtualTask(0.5, 500)
-    competition.scoreAndFtvPrint(FixedTotalValidityStrategy(0.25))
+    competition.tasks = competition.tasks[:-2] # remove latest
+    competition.simulateMinMaxScores(1, 1.0, FixedTotalValidityStrategy(ftv)) # simulate outcomes
+    #competition.addVirtualTask(1.0, 0)
+    #competition.scoreAndFtvPrint(FixedTotalValidityStrategy(ftv))
+
+    # run monte carlo simulation on task results:
+    #method = ['uniform', 'copy_task', 'uniform_max_pb', 'gaussian_pilot'][0]
+    method = ['uniform_max_pb', 'gaussian_pilot'][1]
+    competition.monteCarloSimulation(50000, 1, 1.0, method, FixedTotalValidityStrategy(ftv))
+
+    #file.read()
+    #competition = file.read_competition()
+    #competition.tasks = competition.tasks[:-1] # remove last one    
+    #competition.scoreAndFtvPrint(FixedTotalValidityStrategy(ftv))
+
+    #competition = file.read_competition()
+    #competition.tasks = competition.tasks[:-1] # remove last one
+    #competition.addVirtualTask(1.0, 0)    
+    #competition.scoreAndFtvPrint(FixedTotalValidityStrategy(ftv))
+
+    #competition = file.read_competition()
+    #competition.tasks = competition.tasks[:-1] # remove last one
+    #competition.addVirtualTask(1.0, 1000)    
+    #competition.scoreAndFtvPrint(FixedTotalValidityStrategy(ftv))
+
 
     
 
